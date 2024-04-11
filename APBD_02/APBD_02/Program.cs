b19a0d4fea9a0dfce7ac55de35520c31f20b60e2
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-11 22:41:37 +0200

    Added APBD_02

---
+++ APBD_02/APBD_02/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] tab = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        Console.WriteLine();
+        Console.WriteLine("Średnia: " + Srednia(tab));
+        Console.WriteLine("Max: " + Max(tab));
+
+        Show(tab);
+    }
+
+    static double Srednia(int[] tab)
+    {
+        double sum = 0;
+        foreach (var x in tab)
+            sum += x;
+
+        return sum / tab.Length;
+    }
+    static int Max(int[] tab)
+    {
+        int max = tab[0];
+        foreach (var i in tab)
+            if (i > max)
+                max = i;
+
+        return max;
+    }
+    static void Show(int[] tab)
+    {
+        foreach (var i in tab)
+            Console.Write(i + " ");
+        Console.WriteLine();
+    }
+}
