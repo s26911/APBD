1bcff86a2a76cf0458ddbac4d2620b36d782843e
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-15 13:14:44 +0200

    Initial commit for APBD_06

---
+++ APBD_06/APBD_06/Controllers/AnimalController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_06.Controllers;
+
+[Route("api/animals")]
+[ApiController]
+public class AnimalController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetStudents(string sortBy)
+    {
+        
+        return Ok();
+    }
+}
