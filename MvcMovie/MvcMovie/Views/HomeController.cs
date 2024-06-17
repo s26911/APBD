31f3268d0abb264268d9d01374fc448bc7afb183
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-06-17 13:43:57 +0200

    IC APBD_12

---
+++ MvcMovie/MvcMovie/Views/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Views
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
